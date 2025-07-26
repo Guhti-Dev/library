c0cbb4cc7cc829ed75510b24f60920583322660b
Gustavo <gustavo.aguiar.dev@outlook.com>
2025-07-25 23:53:20 -0300

    [FRONT-001] Correção de Componente Login

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home/home.tsx'
-import Login from './pages/Login/Login.tsx'
+//import Login from './pages/Login/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Home/>
-    <Login/>
   </StrictMode>,
 )
